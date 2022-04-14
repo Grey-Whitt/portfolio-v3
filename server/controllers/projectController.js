733b2bab317cfe80e58e9005a8a451b0d1879884
greyw <greywhitt@gmail.com>
2022-04-14 16:08:29 -0500

    finished project controllers

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -7,10 +7,13 @@ import { Project } from '../models/index.js'
 const getProjects = asyncHandler(async (req, res) => {
   const projects = await Project.findAll({
     attributes: [
+      'id',
       'title',
       'description',
       ['repo_link', 'repoLink'],
       ['deployed_link', 'deployedLink'],
+      'createdAt',
+      'updatedAt',
     ],
   })
 
@@ -53,11 +56,66 @@ const createProject = asyncHandler(async (req, res) => {
 // @desc  Update project
 // @route PUT /api/projects/:id
 // @access Public
-const updateProject = asyncHandler(async (req, res) => {})
+const updateProject = asyncHandler(async (req, res) => {
+  const project = await Project.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+
+  const {
+    title,
+    description,
+    repoLink: repo_link,
+    deployedLink: deployed_link,
+  } = req.body
+
+  if (project) {
+    const updatedProject = await Project.update(
+      {
+        title,
+        description,
+        deployed_link,
+        repo_link,
+      },
+      {
+        plain: true,
+        individualHooks: true,
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+
+    res.status(201).json(updatedProject)
+  } else {
+    res.status(404)
+    throw new Error('Project not found')
+  }
+})
 
 // @desc  delete project
 // @route DELETE /api/projects/:id
 // @access Public
-const deleteProject = asyncHandler(async (req, res) => {})
+const deleteProject = asyncHandler(async (req, res) => {
+  const project = await Project.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+
+  if (project) {
+    await Project.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+  } else {
+    res.status(404)
+    throw new Error('Project not found')
+  }
+
+  res.json({ message: 'Project deleted' })
+})
 
 export { getProjects, createProject, updateProject, deleteProject }
