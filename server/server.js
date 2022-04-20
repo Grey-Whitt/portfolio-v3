25de5acfaa1c6a61f1387df1715b340b6ab4e715
greyw <greywhitt@gmail.com>
2022-04-20 11:59:02 -0500

    added image upload to server with multer

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import sequelize from './config/connection.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import profileRoutes from './routes/profileRoutes.js'
 import projectRoutes from './routes/projectRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.use(express.json())
 app.use('/api/profile', profileRoutes)
 
 app.use('/api/projects', projectRoutes)
+
+app.use('/api/upload', uploadRoutes)
 
 app.use(notFound)
 
