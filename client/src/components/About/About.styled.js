72594300c3dacc91be9e00d6fefa708ddcaf8759
greyw <greywhitt@gmail.com>
2022-04-28 19:30:42 -0500

    re formatted about section

--- client/src/components/About/About.styled.js
+++ client/src/components/About/About.styled.js
@@ -6,9 +6,10 @@ export const AboutContainer = styled.div`
   margin-right: -15px;
   margin-left: -15px;
   justify-content: center !important;
-  /* box-shadow: 0 5px 5px 0 rgb(181 181 181 / 50%), 0 0 0 1px #b5b5b5; */
   box-shadow: 0px 0px 5px 3px rgba(0, 0, 0, 0.34);
   height: 50rem;
+  border-radius: 30px;
+  background-color: ${({ theme }) => theme.colors.jet};
 `
 
 export const AboutLeft = styled.div`
@@ -16,10 +17,11 @@ export const AboutLeft = styled.div`
   max-width: 50%;
   background-color: ${({ theme }) => theme.colors.jet};
   color: ${({ theme }) => theme.colors.lightGray};
+  border-radius: 30px 0 0 30px;
 
   .picContainer {
-    height: 100%;
     width: 100%;
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -27,15 +29,9 @@ export const AboutLeft = styled.div`
 
   .orangeBox {
     border: 5px solid ${({ theme }) => theme.colors.accent};
+    border-radius: 20px;
     height: 80%;
-  }
-
-  h2 {
-    transform: translateY(100%) rotate(-180deg);
-    writing-mode: vertical-lr;
-    margin: 0;
-    font-family: 'IBM Plex Mono', monospace;
-    font-weight: 400;
+    margin: auto;
   }
 `
 
@@ -44,15 +40,28 @@ export const AboutRight = styled.div`
   max-width: 50%;
   border: 20px solid ${({ theme }) => theme.colors.jet};
   box-shadow: inset 0px 0px 7px 5px rgba(0, 0, 0, 0.34);
+  border-radius: 30px;
+  background-color: ${({ theme }) => theme.colors.lightGray};
+
+  .bio {
+    padding: 40px;
+  }
 
   p {
-    padding: 50px;
     color: ${({ theme }) => theme.colors.jet} !important;
-    font-size: 1.2rem;
+    font-size: 1.3rem;
+  }
+
+  h2 {
+    margin: 0;
+    font-family: 'IBM Plex Mono', monospace;
+    font-weight: 600;
+    font-size: 2.5rem;
   }
 `
 
 export const Picture = styled.img`
   max-height: 100%;
   padding: 10px;
+  border-radius: 20px;
 `
