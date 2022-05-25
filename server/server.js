b18f6315d7b731d3f8b75145d99912070b9c25dc
greyw <greywhitt@gmail.com>
2022-05-25 18:45:12 -0500

    added production logic to server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import sequelize from './config/connection.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
@@ -31,3 +32,17 @@ const PORT = process.env.PORT || 5000
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, console.log(`Server running on port ${PORT}`))
 })
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
