fef349bb664be367a4bd9ee5862d44e2af46666c
greyw <greywhitt@gmail.com>
2022-05-25 18:27:23 -0500

    edited heroku postgres connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,7 +6,23 @@ dotenv.config()
 let sequelize
 
 if (process.env.HEROKU_POSTGRES_URI) {
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRES_URI)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  })
+
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.')
+    })
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err)
+    })
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
